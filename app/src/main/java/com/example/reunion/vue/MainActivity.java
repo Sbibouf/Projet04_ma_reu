99436a79b40265e88ef6c1cd9b76d3dfe170999e
Adrien Heral <adrienheral@hotmail.com>
2023-04-28 18:17:23 +0200

    petite correction

--- app/src/main/java/com/example/reunion/vue/MainActivity.java
+++ app/src/main/java/com/example/reunion/vue/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        initViewModel();
+        //initViewModel();
         initUI();
         ajouterReunion();
 
